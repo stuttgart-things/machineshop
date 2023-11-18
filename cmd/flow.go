022266de8766e2e46afb967406e3fc1d1bf9d383
Patrick Hermann <patrick.hermann@sva.de>
2023-11-18 07:07:02 +0000

    updated machineShop 2023.1118.0707 for tag version v0.1.53

---
+++ cmd/flow.go
@@ -0,0 +1,109 @@
+/*
+Copyright © 2023 Patrick Hermann patrick.hermann@sva.de
+*/
+
+package cmd
+
+import (
+	"fmt"
+
+	sthingsBase "github.com/stuttgart-things/sthingsBase"
+	sthingsCli "github.com/stuttgart-things/sthingsCli"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	templatePath = "tests/template.yaml"
+	renderOption = "missingkey=zero"
+)
+
+type Profile struct {
+	Defaults    string `mapstructure:"defaults"`
+	Repository  string `mapstructure:"repository"`
+	Environment string `mapstructure:"environment"`
+}
+
+type Default struct {
+	DefaultProfile Profile `mapstructure:"profile"`
+}
+
+type Binary struct {
+	Url string `mapstructure:"url"`
+	Bin string `mapstructure:"bin"`
+}
+
+type Config struct {
+	BinaryProfile []map[string]Binary `mapstructure:"binaries"`
+}
+
+// flowCmd represents the flow command
+var flowCmd = &cobra.Command{
+	Use:   "flow",
+	Short: "run workflows",
+	Long:  `run workflows based on profiles`,
+
+	Run: func(cmd *cobra.Command, args []string) {
+
+		// READ FLAGS
+		profilePath, _ := cmd.LocalFlags().GetString("profile")
+		var config Config
+		var config1 Default
+		m := make(map[string]int)
+
+		// READ PROFILE
+		log.Info("LOCAL PROFILE FOUND : ", profilePath)
+
+		config1 = sthingsCli.ReadYamlToObject(profilePath, ".yaml", config1).(Default)
+		fmt.Println(config1.DefaultProfile.Defaults)
+
+		config = sthingsCli.ReadYamlToObject(profilePath, ".yaml", config).(Config)
+
+		for i, config := range config.BinaryProfile {
+
+			for binary := range config {
+				m[binary] = i
+			}
+		}
+
+		readInProfile := sthingsBase.ReadFileToVariable(profilePath)
+		// keys := ReadKeysFromYamlDict("environment", "tests/workflow", "yaml")
+		// fmt.Println(keys)
+		// GET ENVIRONMENTS
+
+		templateFile := sthingsBase.ReadFileToVariable(templatePath)
+
+		defaultVariables := sthingsCli.ReadYamlKeyValuesFromFile([]byte(readInProfile))
+		fmt.Println(defaultVariables)
+
+		// hello := defaultVariables["environment"]
+		// fmt.Println(hello)
+		hello := defaultVariables["environment"]
+		fmt.Println(hello)
+
+		// sthingsCli.AskMultiSelectQuestion("SELECT BINARIES TO INSTALL:", bla)
+
+		// bla = defaultVariables["environment"].(string)
+
+		// s := strings.Split(hello.(string), "")
+		// fmt.Println(s)
+
+		// sthingsCli.AskMultiSelectQuestion("SELECT BINARIES TO INSTALL:", strings.Split(hello, ""))
+
+		renderedTemplate, err := sthingsBase.RenderTemplateInline(templateFile, renderOption, brackets["curly"].begin, brackets["curly"].end, defaultVariables)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		fmt.Println(string(renderedTemplate))
+
+		sthingsBase.WriteDataToFile("./hello.yaml", string(renderedTemplate))
+
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(flowCmd)
+	flowCmd.Flags().String("profile", "tests/workflow.yaml", "path to to be used profile")
+
+}
