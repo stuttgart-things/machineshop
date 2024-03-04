a132fc72488abcf967d432de8858d96eb6b8ffff
Patrick Hermann <patrick.hermann@sva.de>
2024-03-04 19:37:44 +0000

    updated imports

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "github.com/stuttgart-things/machineShop/cmd"
+import "github.com/stuttgart-things/machineshop/cmd"
 
 func main() {
 	cmd.Execute()
