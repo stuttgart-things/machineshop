8a25acbd7dee2437a1cc079f31eb972f55d3ecf1
patrick-hermann-sva.de <patrick-hermann-sva.de>
2024-05-19 07:34:39 +0000

    test

--- test/test/main.go
+++ test/test/main.go
@@ -91,23 +91,6 @@ func getTree(ref *github.Reference) (tree *github.Tree, err error) {
 
 // getFileContent loads the local content of a file and return the target name
 // of the file in the target repository and its contents.
-func getFileContent(fileArg string) (targetName string, b []byte, err error) {
-	var localFile string
-	files := strings.Split(fileArg, ":")
-	switch {
-	case len(files) < 1:
-		return "", nil, errors.New("empty `-files` parameter")
-	case len(files) == 1:
-		localFile = files[0]
-		targetName = files[0]
-	default:
-		localFile = files[0]
-		targetName = files[1]
-	}
-
-	b, err = os.ReadFile(localFile)
-	return targetName, b, err
-}
 
 // pushCommit creates the commit in the given reference using the given tree.
 func pushCommit(ref *github.Reference, tree *github.Tree) (err error) {
@@ -218,3 +201,31 @@ func main() {
 }
 
 func ConvertStringToPointer(s string) *string { return &s }
+
+func ReadFileToVar(file string) (content []byte, err error) {
+
+	_, content, err = getFileContent(file)
+	if err != nil {
+		return nil, err
+	}
+
+	return content, nil
+}
+
+func getFileContent(fileArg string) (targetName string, b []byte, err error) {
+	var localFile string
+	files := strings.Split(fileArg, ":")
+	switch {
+	case len(files) < 1:
+		return "", nil, errors.New("empty `-files` parameter")
+	case len(files) == 1:
+		localFile = files[0]
+		targetName = files[0]
+	default:
+		localFile = files[0]
+		targetName = files[1]
+	}
+
+	b, err = os.ReadFile(localFile)
+	return targetName, b, err
+}
