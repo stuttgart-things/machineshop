ffca999dd461124f75bdb92889a8132bdc597052
Patrick Hermann <patrick.hermann@sva.de>
2025-03-04 09:38:23 +0000

    feat: feature/update-go-version

--- ci/main.go
+++ ci/main.go
@@ -29,14 +29,14 @@ var config struct {
 }
 
 type Ci struct {
-	MachineShopContainer *dagger.Container
+	// MachineShopContainer *dagger.Container
 }
 
-// GetMachineShopContainer return the default image for golang
-func (m *Ci) GetMachineShopContainer() *dagger.Container {
-	return dag.Container().
-		From("ghcr.io/stuttgart-things/github.com/stuttgart-things/machineshop:v2.6.3")
-}
+// // GetMachineShopContainer return the default image for golang
+// func (m *Ci) GetMachineShopContainer() *dagger.Container {
+// 	return dag.Container().
+// 		From("ghcr.io/stuttgart-things/github.com/stuttgart-things/machineshop:v2.6.3")
+// }
 
 func New(
 	// machineShop container
@@ -47,32 +47,13 @@ func New(
 ) *Ci {
 	machineShop := &Ci{}
 
-	if machineShopContainer != nil {
-		machineShop.MachineShopContainer = machineShopContainer
-	} else {
-		machineShop.MachineShopContainer = machineShop.GetMachineShopContainer()
-	}
+	// if machineShopContainer != nil {
+	// 	machineShop.MachineShopContainer = machineShopContainer
+	// } else {
+	// 	machineShop.MachineShopContainer = machineShop.GetMachineShopContainer()
+	// }
 
 	return machineShop
-}
-
-// Test machineShop version command
-func (m *Ci) TestVersion(ctx context.Context) (versionOutput string) {
-
-	fmt.Println("RUNNING VERSION COMMAND...")
-
-	versionOutput, err := m.MachineShopContainer.
-		WithExec(
-			[]string{"machineshop", "version"}).
-		Stdout(ctx)
-
-	if err != nil {
-		fmt.Println("ERROR RUNNING VERSION COMMAND: ", err)
-	}
-
-	fmt.Println(versionOutput)
-
-	return versionOutput
 }
 
 func (m *Ci) BuildAndTestInstallCmd(
